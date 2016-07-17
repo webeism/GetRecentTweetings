5ba3d1f23b2b247add8d9d4399396d99136fd78a
webeism <john@webeism.co.uk>
2016-07-17 19:12:14 +0100

    removed functions include

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 //Get config params
 require_once("inc/config.php");
-//Get functions
-require_once("inc/functions.php");
 //Get class
 require_once("inc/clsTweetings.php");
 //Initiate instance
 $oTweetings = new clsTweetings();
-//Do something
+//Display the results
 echo $oTweetings -> fnDisplay();
 ?>
