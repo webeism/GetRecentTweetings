73718665d2acc800fa8085921aeb75c03b8298a9
webeism <john@webeism.co.uk>
2016-07-17 19:48:46 +0100

    Trying to get heroku env vars working

--- inc/config.php
+++ inc/config.php
@@ -2,13 +2,13 @@
 // Put your twitter credentials here in place of the xxx's... 
 
 // "Consumer key"
-if(!getenv("CONSUMERKEY")){
+if(getenv("CONSUMERKEY")===false){
 	define("CONSUMERKEY","xxx");
 }else{
 	define("CONSUMERKEY",getenv("CONSUMERKEY"));
 }
 // "Consumer secret"
-if(!getenv("CONSUMERKEYSECRET")){
+if(getenv("CONSUMERKEYSECRET")===false){
 	define("CONSUMERKEYSECRET","xxx");
 }else{
 	define("CONSUMERKEY",getenv("CONSUMERKEYSECRET"));	
